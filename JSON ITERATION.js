76c6e46ef6ea3a1fda9021f53271dbf79beb151f
Pramod Reddy Seri <48101140+seripramodreddy@users.noreply.github.com>
2020-11-05 09:01:01 +0530

    Update JSON ITERATION.js

--- JSON ITERATION.js
+++ JSON ITERATION.js
@@ -274,3 +274,28 @@ var securityQuestions = [
   function isObject(object) {
     return object != null && typeof object === 'object';
   }
+
+
+//Write a function to return the list of characters below 20 age
+    var students = [
+        {
+        name: "Siddharth Abhimanyu", age: 21}, { name: "Malar", age: 25},
+        {name: "Maari",age: 18},{name: "Bhallala Deva",age: 17},
+        {name: "Baahubali",age: 16},{name: "AAK chandran",age: 23},   {name:"Gabbar Singh",age: 33},{name: "Mogambo",age: 53},
+        {name: "Munnabhai",age: 40},{name: "Sher Khan",age: 20},
+        {name: "Chulbul Pandey",age: 19},{name: "Anthony",age: 28},
+        {name: "Devdas",age: 56} 
+        ];
+var obj={};
+for( i in students)
+{   
+
+    if(students[i].age<20)
+    {
+         obj[i]=students[i];
+    }
+    
+}
+console.log(obj);
+
+
