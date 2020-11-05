7cf0e69bc9a1f3301cf4aa60cbd831d67a9a7765
Pramod Reddy Seri <48101140+seripramodreddy@users.noreply.github.com>
2020-11-05 08:58:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var request= new XMLHttpRequest();
+
+request.open('GET','https://restcountries.eu/rest/v2/all',true);
+
+request.send();
+
+request.onload=function(){
+    var data=JSON.parse(this.response);
+    for( i in data)
+    {
+        console.log(data[i].flag);
+    }
+    //console.log(data[0]);
+}
